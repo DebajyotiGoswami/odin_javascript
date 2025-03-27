e8cc1bb2492a04d44f70c47683626ca2bf1d58df
Debajyoti Goswami <goswamidiary@gmail.com>
2025-03-27 16:12:12 +0530

    new function getHumanChoice created

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 console.log("hello world from external javascript file");
 
 function getComputerChoice(){
+    //return one of rock, paper and scissors
     let choice = ['rock' , 'paper', 'scissors'];
     let randomIndex = Math.floor(Math.random()* choice.length)
 
     return choice[randomIndex];
 }
 
+function getHumanChoice(){
+    //get input from human
+    choice = prompt("What is your choice: ROCK / PAPER / SCISSORS ? ");
+    console.log(choice);
+}
+
+console.log(getHumanChoice());
 console.log(getComputerChoice());
